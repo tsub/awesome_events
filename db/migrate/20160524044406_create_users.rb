a906973c92b9a0b673d31ff05e3782101d2b5703
tsub <tsubasatakayama511@gmail.com>
2016-05-24 13:48:50 +0900

    本に従ってtimestampsのNOT NULL制約を削除

--- db/migrate/20160524044406_create_users.rb
+++ db/migrate/20160524044406_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :nickname,  null: false
       t.string :image_url, null: false
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
