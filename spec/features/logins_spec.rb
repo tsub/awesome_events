8fb738bd54c3b14420db516c3ce6d8d20cc3ee8a
tsub <tsubasatakayama511@gmail.com>
2016-05-26 12:37:28 +0900

    ユーザーがTwitterでログインに成功した後にトップページにリダイレクトされることを期待したい

--- spec/features/logins_spec.rb
+++ spec/features/logins_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature "Logins", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ユーザーがイベントの参加や登録を行うために、ログインをする' do
+    context 'かつTwitterでのログインに成功した時' do
+      before do
+        visit root_path
+        click_link 'Twitterでログイン'
+      end
+
+      it 'トップページに遷移していること' do
+        expect(page.current_path).to eq root_path
+      end
+  end
 end
