f8215d1757fe1479836288e7294cfcaff41306c2
tsub <tsubasatakayama511@gmail.com>
2016-05-27 11:57:19 +0900

    Event#placeが空白または100文字より長い場合バリデーションエラーを出したい

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Event, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(50) }
   end
+
+  describe '#place' do
+    it { should validate_presence_of(:place) }
+    it { should validate_length_of(:place).is_at_most(100) }
+  end
 end
