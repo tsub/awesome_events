0ee8ac663586950dba17c28ab7ee2dcc0750002e
tsub <tsubasatakayama511@gmail.com>
2016-05-27 11:37:33 +0900

    shoulda-matchersを使ってバリデーション周りのテストを簡潔にした

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,49 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   describe '#name' do
-    context 'nilの時' do
-      let(:event) { Event.new(name: nil) }
-
-      it 'validでないこと' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
-
-    context '空白の時' do
-      let(:event) { Event.new(name: '') }
-
-      it 'validでないこと' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
-
-    context '"Rails勉強会"の時' do
-      let(:event) { Event.new(name: 'Rails勉強会') }
-
-      it 'validであること' do
-        event.valid?
-        expect(event.errors[:name]).to be_blank
-      end
-    end
-
-    context '50文字の時' do
-      let(:event) { Event.new(name: 'a' * 50) }
-
-      it 'validであること' do
-        event.valid?
-        expect(event.errors[:name]).to be_blank
-      end
-    end
-
-    context '51文字の時' do
-      let(:event) { Event.new(name: 'a' * 51) }
-
-      it 'validでないこと' do
-        event.valid?
-        expect(event.errors[:name]).to be_present
-      end
-    end
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(50) }
   end
 end
