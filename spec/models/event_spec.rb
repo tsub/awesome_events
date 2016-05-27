39e6bc52017bfcbcf4d2892ce721a147785de521
tsub <tsubasatakayama511@gmail.com>
2016-05-27 11:22:38 +0900

    Event#nameがnilの時も同様にvalidationエラーになること

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   describe '#name' do
+    context 'nilの時' do
+      let(:event) { Event.new(name: nil) }
+
+      it 'validでないこと' do
+        event.valid?
+        expect(event.errors[:name]).to be_present
+      end
+    end
+
     context '空白の時' do
       let(:event) { Event.new(name: '') }
 
