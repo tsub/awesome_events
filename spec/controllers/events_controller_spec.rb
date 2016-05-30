c980ac00372472248fae10596099b076a3ae774f
tsub <tsubasatakayama511@gmail.com>
2016-05-30 11:48:21 +0900

    ユーザーがログインしていなかったらトップページにリダイレクトさせたい

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,12 +2,27 @@ require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
   describe 'GET #new' do
-    before do
-      get :new
+    context 'ログインユーザーがアクセスした時' do
+      before do
+        user = create(:user)
+        session[:user_id] = user.id
+
+        get :new
+      end
+
+      it 'ステータスコードとして200が返ること' do
+        expect(response).to have_http_status(200)
+      end
     end
 
-    it 'ステータスコードとして200が返ること' do
-      expect(response).to have_http_status(200)
+    context '未ログインユーザーがアクセスした時' do
+      before do
+        get :new
+      end
+
+      it 'トップページにリダイレクトさせること' do
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 end
