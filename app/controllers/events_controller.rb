5038bfe05447d5f04e7d2861fe9f378e54b7b3d1
tsub <tsubasatakayama511@gmail.com>
2016-05-30 12:33:51 +0900

    ログインユーザーの取得を共通化

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   before_action :authenticate
 
   def new
-    @event = User.find(session[:user_id]).created_events.build
+    @event = current_user.created_events.build
     head :ok
   end
 end
