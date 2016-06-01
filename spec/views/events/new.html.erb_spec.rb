a07a507be261ae83a04287ff38525761fb47dded
tsub <tsubasatakayama511@gmail.com>
2016-06-01 12:38:13 +0900

    フォームの入力に誤りがあった場合にエラーが表示されるようにした

--- spec/views/events/new.html.erb_spec.rb
+++ spec/views/events/new.html.erb_spec.rb
@@ -10,5 +10,19 @@ RSpec.describe "events/new", type: :view do
     it '「イベント作成」という見出しが含まれたトップページが表示されること' do
       expect(rendered).to match /イベント作成/
     end
+
+    context 'かつ入力した内容にエラーがあった時' do
+      let(:event) { build(:event, :has_invalid_end_time) }
+
+      before do
+        event.valid?
+        assign(:event, event)
+        render
+      end
+
+      it 'エラーメッセージが表示されていること' do
+        expect(rendered).to match /#{event.errors.full_messages.first}/
+      end
+    end
   end
 end
