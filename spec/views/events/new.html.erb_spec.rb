b59b264659e5bfc860c4a165adc9af3c5f9b7310
tsub <tsubasatakayama511@gmail.com>
2016-06-01 11:09:15 +0900

    イベント新規作成用のフォームを表示するようにした

--- spec/views/events/new.html.erb_spec.rb
+++ spec/views/events/new.html.erb_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "events/new", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'ログインユーザーがアクセスした時' do
+    before do
+      assign(:event, create(:event))
+      render
+    end
+
+    it '「イベント作成」という見出しが含まれたトップページが表示されること' do
+      expect(rendered).to match /イベント作成/
+    end
+  end
 end
