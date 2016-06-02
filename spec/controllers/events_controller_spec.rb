9330123bbb62a4de9f587225946122d19df8abff
tsub <tsubasatakayama511@gmail.com>
2016-06-02 12:43:25 +0900

    urlパラメータで指定したidのイベント情報を返すようにした

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -16,6 +16,15 @@ RSpec.describe EventsController, type: :controller do
         expect(response).to have_http_status(200)
       end
 
+      it '@eventにurlパラメータに渡したidのEventオブジェクトが格納されていること' do
+        expect(assigns(:event).id).to eq event.id
+        expect(assigns(:event).name).to eq event.name
+        expect(assigns(:event).place).to eq event.place
+        expect(assigns(:event).content).to eq event.content
+        expect(assigns(:event).start_time).to eq event.start_time
+        expect(assigns(:event).end_time).to eq event.end_time
+      end
+
       it 'showテンプレートをrenderしていること' do
         expect(response).to render_template(:show)
       end
