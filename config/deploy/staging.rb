1f006507ab6140ebaf3aca48909ad7e3b87ab94a
tsub <tsubasatakayama511@gmail.com>
2016-06-07 15:47:57 +0900

    Configure staging setting with capistrano

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -20,7 +20,9 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-
+role :app, %w{ops@awesome_events.local}
+role :web, %w{ops@awesome_events.local}
+role :db,  %w{ops@awesome_events.local}
 
 
 # Configuration
