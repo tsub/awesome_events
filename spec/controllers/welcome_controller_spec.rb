78e240394efe1aca49b1fc1a3465fd66a7b6f324
tsub <tsubasatakayama511@gmail.com>
2016-06-07 11:08:44 +0900

    一旦welcome#indexに期待するテスト項目を書いた

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
+  describe '#index' do
+    context '複数のイベント情報が登録されている時' do
+      it 'イベント情報を開催時間の昇順で返すこと'
 
+      it '未開催のイベント情報のみを返すこと'
+    end
+  end
 end
