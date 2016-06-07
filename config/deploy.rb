00ce97d8f4f9d8355d258414df62cc35d1ec83bc
tsub <tsubasatakayama511@gmail.com>
2016-06-07 15:47:39 +0900

    Configure capistrano deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,19 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'awesome_events'
+set :repo_url, 'git@github.com:tsub/awesome_events.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/awesome_events'
 
 # Default value for :scm is :git
 # set :scm, :git
+set :scm, :git
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -34,6 +36,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
