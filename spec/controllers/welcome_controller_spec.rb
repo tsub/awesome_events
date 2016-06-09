6e4b20d605470ffcbfd030327aa1b4ca42b61378
tsub <tsubasatakayama511@gmail.com>
2016-06-09 12:50:07 +0900

    travel_toできちんとコントローラー側と時刻を同期できていなかったため修正

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
   describe '#index' do
-    before do
-      get :index
-    end
-
     context '複数のイベント情報が登録されている時' do
       let(:events) { create_list(:event, 10) }
       let(:include_closed_events) { create_list(:event, 10, :has_closed_start_time).concat(events) }
       let(:filtered_events) { include_closed_events.select { |event| event.start_time > Time.zone.now } }
+
+      before do
+        get :index
+      end
 
       it 'イベント情報を開催時間の昇順で返すこと' do
         expect(assigns(:events)).to match filtered_events.sort { |a, b| a.start_time <=> b.start_time }
@@ -21,6 +21,10 @@ RSpec.describe WelcomeController, type: :controller do
     end
 
     context 'イベントが登録されていなかった時' do
+      before do
+        get :index
+      end
+
       it '空のイベント情報を返すこと' do
         expect(assigns(:events)).to be_empty
       end
@@ -30,6 +34,7 @@ RSpec.describe WelcomeController, type: :controller do
       before do
         travel_to Time.zone.local(2016, 1, 1, 0, 0, 0) do
           create(:event, :has_start_time_is_now)
+          get :index
         end
       end
 
@@ -41,6 +46,7 @@ RSpec.describe WelcomeController, type: :controller do
     context '現在時刻より古い開催時間のイベントしか登録されていなかった時' do
       before do
         create(:event, :has_closed_start_time)
+        get :index
       end
 
       it '空のイベント情報を返すこと' do
